e4e803312e30ab283af7c6417bdb943dd6cac703
mattfox <mattfox@mit.edu>
2014-11-20 20:07:29 -0500

    Added multiple databases opened and closed

--- db.cpp
+++ db.cpp
@@ -14,13 +14,19 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
 
 
 int main(){
-    sqlite3 *db;
+    sqlite3 *db1, *db2, *db3, *db4;
+    int rc1, rc2, rc3, rc4;
     char *sql;
     char *zErrMsg = 0;
+    
 
-    int rc = sqlite3_open("test.db", &db);
-    if( rc ){
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    rc1 = sqlite3_open("test1.db", &db1);
+    rc2 = sqlite3_open("test2.db", &db2);
+    rc3 = sqlite3_open("test3.db", &db3);
+    rc4 = sqlite3_open("test4.db", &db4);
+
+    if( rc1 ){
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db1));
 	exit(0);
     }else{
 	fprintf(stderr, "Opened database successfully\n");
@@ -35,13 +41,18 @@ int main(){
 	  "SALARY         REAL );";
 
      /* Execute SQL statement */
-    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-    if( rc != SQLITE_OK ){
+    rc1 = sqlite3_exec(db1, sql, callback, 0, &zErrMsg);
+    if( rc1 != SQLITE_OK ){
         fprintf(stderr, "SQL error: %s\n", zErrMsg);
 	sqlite3_free(zErrMsg);
     }else{
 	fprintf(stdout, "Table created successfully\n");
     }
-    sqlite3_close(db);
+    sqlite3_close(db1);
+    sqlite3_close(db2);
+    sqlite3_close(db3);
+    sqlite3_close(db4);
+
+
     return 0;
 }
