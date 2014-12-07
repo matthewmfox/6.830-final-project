eed92e209c408b314824f530fde82568e5778610
mattfox <mattfox@mit.edu>
2014-12-07 10:32:56 -0500

    Added file for streamer

--- Makefile
+++ Makefile
@@ -5,9 +5,13 @@ CC = g++
 CFLAGS = -l sqlite3
 DEPS =
 OBJ = db.o
+OBJ2 = twitterStream.o
 
 %.o: %.cpp $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
 
 db: $(OBJ)
+	g++ -o $@ $^ $(CFLAGS)
+	
+twitterStream: $(OBJ2)
 	g++ -o $@ $^ $(CFLAGS)
---
+++ twitterStream.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include "include/twitcurl.h"
+#include <string>
+using namespace std;
+
+void printUsage()
+{
+    printf( "\nUsage:\ntwitterClient -u username -p password\n" );
+}
+
+int main(){
+    string userName( "moxiefoxxx" );
+    string passWord( "6830pass" );
+
+    printUsage();
+
+}
